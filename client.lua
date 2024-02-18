66e1262f15d81fc9e33544390ead22bc27716e19
ByPlanix <93527341+ByPlanix@users.noreply.github.com>
2024-02-18 22:44:54 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,21 +1,7 @@
-local locked = false
+AddEventHandler('EZELS:OnLocalPlayerEnteredElsVehicle', function()
+    LocalPlayer.state.invHotkeys = false
+end)
 
-Citizen.CreateThread(function()
-    while true do
-        local ped = GetPlayerPed(-1)
-        if IsPedInAnyVehicle(ped, false) then
-            local vehicle = GetVehiclePedIsIn(ped, false)
-            local class = GetVehicleClass(vehicle)
-            if class == 18 and locked == false then
-                locked = true
-                LocalPlayer.state.invHotkeys = false
-            end
-        else
-            if locked == true then
-                locked = false
-                LocalPlayer.state.invHotkeys = true
-            end
-        end
-        Wait(1000)
-    end
+AddEventHandler('EZELS:OnLocalPlayerExitedElsVehicle', function()
+    LocalPlayer.state.invHotkeys = true
 end)
